c84421b9bde7c217700619e92bdcf5d0e2acba61
Dimas Mahbiali <dimasmahbialay@gmail.com>
2016-10-10 23:08:07 +0700

    5-2
    5-2

---
+++ _5-2.ino
@@ -0,0 +1,12 @@
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  Serial.write(0xf5);
+  Serial.write(0xff);
+  Serial.write(0x3f);
+  Serial.write(0xf3);
+  Serial.write(0x44);
+  delay(2000);
+}
